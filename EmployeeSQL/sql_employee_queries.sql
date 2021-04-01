a63f5a336342c89c6fadda3d9e9117b8bef0c764
wagneree193 <wagneree193@gmail.com>
2021-04-01 16:27:15 -0500

    tried hire date query, needs work

--- EmployeeSQL/sql_employee_queries.sql
+++ EmployeeSQL/sql_employee_queries.sql
@@ -7,4 +7,14 @@ FROM "Employee" as emp
 JOIN "Salaries" as sal
 on emp.emp_no = sal.salary_id;
 
+-- checking format of hire date
+SELECT hire_date
+FROM "Employee";
+
+-- list first name, last name and hire date for employees hired in 1986
+SELECT CONVERT (VARCHAR, hire_date);
+
+SELECT last_name, first_name, hire_date
+FROM "Employee"
+WHERE CONVERT(VARCHAR, hire_date);
 
