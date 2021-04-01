e81f8ae5dc462fac3c825750fe6863df129f8815
wagneree193 <wagneree193@gmail.com>
2021-04-01 16:28:14 -0500

    hire date query

--- EmployeeSQL/sql_employee_queries.sql
+++ EmployeeSQL/sql_employee_queries.sql
@@ -18,3 +18,11 @@ SELECT last_name, first_name, hire_date
 FROM "Employee"
 WHERE CONVERT(VARCHAR, hire_date);
 
+
+--list the manager of each department with department number, department name, mgr employee number, last name, first name
+SELECT dep.dept_no, dep.dept_name, mgr.emp_no, emp.last_name, emp.first_name
+FROM "Department" as dep
+JOIN "Department_manager" as mgr
+ON dep.dept_no = mgr.dept_no
+JOIN "Employee" as emp
+ON mgr.emp_no = emp.emp_no
